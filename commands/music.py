bb86e413a4b61287a26b2a1ca40ece501b120074
Marton Szegedi <martonszegedi2@gmail.com>
2026-01-26 14:51:43 +0200

    fix: yt fix#3

--- commands/music.py
+++ commands/music.py
@@ -22,7 +22,7 @@ ytdl_format_options = {
     'source_address': '0.0.0.0',
 }
 
-FFMPEG_OPTIONS = {
+ffmpeg_options = {
     'before_options': (
         '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 '
         '-user_agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"'
