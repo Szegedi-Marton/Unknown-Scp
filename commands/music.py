6a8100e09a3c829eb216c74061eef3b310a949f0
Marton Szegedi <martonszegedi2@gmail.com>
2026-01-26 14:50:52 +0200

    fix: yt fix#2

--- commands/music.py
+++ commands/music.py
@@ -22,9 +22,12 @@ ytdl_format_options = {
     'source_address': '0.0.0.0',
 }
 
-ffmpeg_options = {
+FFMPEG_OPTIONS = {
+    'before_options': (
+        '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 '
+        '-user_agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"'
+    ),
     'options': '-vn',
-    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5'
 }
 
 ytdl = yt_dlp.YoutubeDL(ytdl_format_options)
