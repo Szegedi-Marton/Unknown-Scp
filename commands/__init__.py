f0fa3a2b44829e4794ec0709491e3e1f2e850cab
Marton Szegedi <martonszegedi2@gmail.com>
2026-01-28 11:24:08 +0200

    feat: adding commands to bot tree

--- commands/__init__.py
+++ commands/__init__.py
@@ -4,6 +4,11 @@ from .moderation import setup_moderation
 from .filter import setup_filter
 from .about import setup_about
 from .music import setup_play
+from .polls import setup_polls
+from .countdown import setup_countDwn
+from .reminder import setup_reminder
+from .user_info import setup_userInf
+
 
 async def register_commands(bot):
     # Await these because bot.add_cog is now a coroutine
@@ -13,3 +18,7 @@ async def register_commands(bot):
     await setup_filter(bot)
     await setup_about(bot)
     await setup_play(bot)
+    await setup_reminder(bot)
+    await setup_countDwn(bot)
+    await setup_polls(bot)
+    await setup_userInf(bot)
